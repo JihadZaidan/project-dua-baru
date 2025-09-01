4c269c6e8dc99d44bfb90b912d467d7127864916
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-01 11:31:46 +0700

    feat(animations): improve gsap animate effect

--- src/components/landing-page/features.tsx
+++ src/components/landing-page/features.tsx
@@ -1,23 +1,49 @@
+"use client"
 import { Button } from "../ui/button"
 import { ArrowRight } from "lucide-react"
 import Image from "next/image"
 import Chitato from "../../../public/landing-page/header/chipschapa.png"
 import Studio from "../../../public/landing-page/header/shooting.png"
 import Bin from "../../../public/landing-page/body/binary.png"
+import { useRef } from "react"
+import { useSlideFromLeft, useSlideFromRight, useFadeIn } from "@/library/animations"
 
 export default function Marketing() {
+
+    const firstoRef = useRef<HTMLDivElement>(null);
+    const dousRef = useRef<HTMLDivElement>(null);
+    const tresseruRef = useRef<HTMLDivElement>(null);
+    const fourtRef = useRef<HTMLDivElement>(null);
+    const onetownRef = useRef<HTMLHeadingElement>(null);
+    const twonRef = useRef<HTMLHeadingElement>(null);
+    const tresuRef = useRef<HTMLHeadingElement>(null);
+    const fourteRef = useRef<HTMLHeadingElement>(null);
+    const headerRef = useRef<HTMLHeadingElement>(null);
+    const parRef = useRef<HTMLParagraphElement>(null);
+    const imageRef = useRef<HTMLDivElement>(null);
+
+    useSlideFromLeft(onetownRef, 0.3);
+    useSlideFromLeft(twonRef, 0.3);
+    useSlideFromLeft(tresuRef, 0.3);
+    useSlideFromLeft(fourteRef, 0.3);
+    useSlideFromRight(firstoRef, 0.3);
+    useSlideFromRight(dousRef, 0.3);
+    useSlideFromRight(tresseruRef, 0.3);
+    useSlideFromRight(fourtRef, 0.3);
+    useFadeIn(imageRef, 0.3);
+
     return (
         <div className="max-w-full w-full lg:px-20 lg:pt-24 lg:pb-28 px-10 py-14 bg-neutral-900">
             <div className="w-full flex flex-col gap-24">
                 {/* Intro */}
                 <div className="w-full flex lg:flex-row flex-col lg:justify-between justify-start items-start gap-10">
-                    <h3 className="text-neutral-100 text-left leading-[140%] lg:text-5xl text-3xl font-sans font-medium">
+                    <h3 ref={headerRef} className="text-neutral-100 text-left leading-[140%] lg:text-5xl text-3xl font-sans font-medium">
                         Transforming Digital <br />
                         Innovations for <br />
                         Visionary Brands
                     </h3>
 
-                    <p className="lg:mt-[135px] text-neutral-100 text-left leading-[180%] text-lg font-sans font-medium">
+                    <p ref={parRef} className="lg:mt-[135px] text-neutral-100 text-left leading-[180%] text-lg font-sans font-medium">
                         We are passionate about uncovering the best digital innovations for forward — <br />
                         thinking brands looking to push boundaries and drive significant impact.
                     </p>
@@ -26,16 +52,18 @@ export default function Marketing() {
                 {/* Section 1 */}
                 <div className="w-full flex flex-col gap-8 border-b border-neutral-300 lg:pb-5">
                     <div className="w-full flex lg:flex-row flex-col lg:justify-between justify-start items-start gap-3">
-                        <h3 className="text-neutral-100 font-sans font-medium lg:text-5xl text-2xl text-left">
+                        <h3 ref={onetownRef} className="text-neutral-100 font-sans font-medium lg:text-5xl text-2xl text-left">
                             01/ DIGITAL STRATEGY.
                         </h3>
-                        <Button className="ml-[-15px] w-fit justify-start gap-2 px">
-                            <p className="text-neutral-100 text-lg font-medium">Explore</p>
-                            <ArrowRight height={20} width={20} className="text-neutral-100" />
-                        </Button>
+                        <div ref={firstoRef}>
+                            <Button className="ml-[-15px] w-fit justify-start gap-2 px">
+                                <p className="text-neutral-100 text-lg font-medium">Explore</p>
+                                <ArrowRight height={20} width={20} className="text-neutral-100" />
+                            </Button>
+                        </div>
                     </div>
 
-                    <div className="w-full flex lg:flex-row flex-col lg:gap-8 gap-6 justify-start items-start">
+                    <div ref={imageRef} className="w-full flex lg:flex-row flex-col lg:gap-8 gap-6 justify-start items-start">
                         <Image
                             src={Chitato}
                             alt="snack packaging design"
@@ -57,39 +85,45 @@ export default function Marketing() {
                 {/* Section 2 */}
                 <div className="w-full flex flex-col gap-5 border-b border-neutral-300 lg:pb-12 pb-4">
                     <div className="w-full flex lg:flex-row flex-col lg:justify-between justify-start items-start gap-3">
-                        <h3 className="text-neutral-100 font-sans font-medium lg:text-5xl text-2xl text-left">
+                        <h3 ref={twonRef} className="text-neutral-100 font-sans font-medium lg:text-5xl text-2xl text-left">
                             02/ BRANDING.
                         </h3>
-                        <Button className="ml-[-15px] w-fit justify-start gap-2 px">
-                            <p className="text-neutral-100 text-lg font-medium">Learn More</p>
-                            <ArrowRight height={20} width={20} className="text-neutral-100" />
-                        </Button>
+                        <div ref={dousRef}>
+                            <Button className="ml-[-15px] w-fit justify-start gap-2 px">
+                                <p className="text-neutral-100 text-lg font-medium">Learn More</p>
+                                <ArrowRight height={20} width={20} className="text-neutral-100" />
+                            </Button>
+                        </div>
                     </div>
                 </div>
 
                 {/* Section 3 */}
                 <div className="w-full flex flex-col gap-5 border-b border-neutral-300 lg:pb-12 pb-4">
                     <div className="w-full flex lg:flex-row flex-col lg:justify-between justify-start items-start gap-3">
-                        <h3 className="text-neutral-100 font-sans font-medium lg:text-5xl text-2xl text-left">
+                        <h3 ref={tresuRef} className="text-neutral-100 font-sans font-medium lg:text-5xl text-2xl text-left">
                             03/ WEB DEVELOPMENT.
                         </h3>
-                        <Button className="ml-[-15px] w-fit justify-start gap-2 px">
-                            <p className="text-neutral-100 text-lg font-medium">Learn More</p>
-                            <ArrowRight height={20} width={20} className="text-neutral-100" />
-                        </Button>
+                        <div ref={tresseruRef}>
+                            <Button className="ml-[-15px] w-fit justify-start gap-2 px">
+                                <p className="text-neutral-100 text-lg font-medium">Learn More</p>
+                                <ArrowRight height={20} width={20} className="text-neutral-100" />
+                            </Button>
+                        </div>
                     </div>
                 </div>
 
                 {/* Section 4 */}
                 <div className="w-full flex flex-col gap-5 border-b border-neutral-300 lg:pb-12 pb-4">
                     <div className="w-full flex lg:flex-row flex-col lg:justify-between justify-start items-start gap-3">
-                        <h3 className="text-neutral-100 font-sans font-medium lg:text-5xl text-2xl text-left">
+                        <h3 ref={fourteRef} className="text-neutral-100 font-sans font-medium lg:text-5xl text-2xl text-left">
                             04/ APP DEVELOPMENT.
                         </h3>
-                        <Button className="ml-[-15px] w-fit justify-start gap-2 px">
-                            <p className="text-neutral-100 text-lg font-medium">Learn More</p>
-                            <ArrowRight height={20} width={20} className="text-neutral-100" />
-                        </Button>
+                        <div ref={fourteRef}>
+                            <Button className="ml-[-15px] w-fit justify-start gap-2 px">
+                                <p className="text-neutral-100 text-lg font-medium">Learn More</p>
+                                <ArrowRight height={20} width={20} className="text-neutral-100" />
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
