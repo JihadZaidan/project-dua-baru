ba06ae8657b609a0e1f5c7c7cd8d592d4d466dc7
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-02 11:02:46 +0700

    feat(animate): upper expertise added effect

--- src/components/expertise/uppertise.tsx
+++ src/components/expertise/uppertise.tsx
@@ -5,6 +5,8 @@ import { motion } from "framer-motion";
 import Kopi from "../../../public/expertise/coffes.png";
 import Tamar from "../../../public/expertise/brands.png";
 import Cody from "../../../public/expertise/coder.png";
+import { useSlideFromTop, useSlideFromLeft, useSlideFromRight } from "@/library/animations";
+import { useRef } from "react";
 
 const images = [
     { src: Kopi, alt: "coffeship", scale: "scale-[50%]" },
@@ -13,11 +15,19 @@ const images = [
 ];
 
 export default function Uppertise() {
+    const slideRef = useRef<HTMLDivElement>(null);
+    const headRef = useRef<HTMLHeadingElement>(null);
+    const pagaRef = useRef<HTMLParagraphElement>(null);
+
+    useSlideFromLeft(headRef, 0.3);
+    useSlideFromRight(pagaRef, 0.3);
+    useSlideFromTop(slideRef, 0.3);
+    
     return (
         <div className="max-w-full w-full lg:px-20 lg:pt-12 lg:pb-18 px-5 pt-6 pb-14">
             <div className="w-full flex flex-col justify-left gap-20">
                 {/* Manual horizontal overflow (no auto) */}
-                <div className="w-full overflow-x-auto scroll-smooth snap-x snap-mandatory [-ms-overflow-style:none] [scrollbar-width:none]"
+                <div ref={slideRef} className="w-full overflow-x-auto scroll-smooth snap-x snap-mandatory [-ms-overflow-style:none] [scrollbar-width:none]"
                     style={{ WebkitOverflowScrolling: "touch" }}>
                     {/* hide scrollbar (optional) */}
                     <style jsx>{`div::-webkit-scrollbar { display: none; }`}</style>
@@ -40,10 +50,10 @@ export default function Uppertise() {
 
                 {/* Text Section */}
                 <div className="w-full flex lg:flex-row flex-col lg:justify-between justify-left items-left gap-10">
-                    <h3 className="text-neutral-800 font-sans font-medium lg:text-5xl text-3xl text-left leading-[140%] pl-5">
+                    <h3 ref={headRef} className="text-neutral-800 font-sans font-medium lg:text-5xl text-3xl text-left leading-[140%] pl-5">
                         Few of Our <br /> Specialites
                     </h3>
-                    <p className="text-neutral-800 font-sans font-medium lg:text-3xl text-base leading-[140%] pl-5">
+                    <p ref={pagaRef} className="text-neutral-800 font-sans font-medium lg:text-3xl text-base leading-[140%] pl-5">
                         — From creative projects that push the <br /> boundaries of our
                         team&apos;s imagination. <br /> Build it into all of our works.
                     </p>
