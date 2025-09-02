acd01cc75faaf4a7bf208290862ef62feb65a713
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-02 09:30:42 +0700

    feat(animate): header effect and paragraph

--- src/components/works/headscript.tsx
+++ src/components/works/headscript.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+import { useRef } from "react"
+import { useSlideFromLeft, useSlideFromRight } from "@/library/animations"
+
 export default function HeaderWork() {
+
+    const headRef = useRef<HTMLHeadingElement>(null);
+    const graphyRef = useRef<HTMLParagraphElement>(null);
+    useSlideFromLeft(headRef, 0.3);
+    useSlideFromRight(graphyRef, 0.3);
+
     return (
         <div className="max-w-full w-full bg-neutral-100 lg:px-20 lg:pt-16 lg:pb-20 px-8 pt-6 pb-14">
             <div className="w-full flex flex-col lg:gap-25 gap-16">
-                <h1 className="font-sans font-medium text-left lg:text-7xl text-4xl text-neutral-800 leading-[130%]">Check Our <br /> Works</h1>
-                <p className="font-sans font-medium lg:text-right lg:w-full lg:text-3xl text-base text-neutral-800 leading-[150%]">
+                <h1 ref={headRef} className="font-sans font-medium text-left lg:text-7xl text-4xl text-neutral-800 leading-[130%]">Check Our <br /> Works</h1>
+                <p ref={graphyRef} className="font-sans font-medium lg:text-right lg:w-full lg:text-3xl text-base text-neutral-800 leading-[150%]">
                     — Dive into Our Works and Explore <br />
                     Transformative Digital Strategies, <br />
                     Branding Initiatives, and Innovative Web <br />
